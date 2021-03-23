ff1fb2ff3603a76f39bbedc7b70502a1eeef0aca
student <Student@B420-01.fesb.hr>
2021-03-23 19:59:57 +0100

    Drugi commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Novo");
+
         }
     }
 }
